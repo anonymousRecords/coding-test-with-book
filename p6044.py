0c9ff5935deb10b1558f8ef2046acbccc51b9d41
anonymousRecords <a.Records2022@gmail.com>
2022-08-21 19:35:00 +0900

    p6041 ~ p6044

---
+++ p6044.py
@@ -0,0 +1,11 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a+b)
+print(a-b)
+print(a*b)
+division = a / b
+print(int(division))
+print(a%b)
+float = float(a)/float(b)
+print(format(float, ".2f"))
