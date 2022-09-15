7618ad83bb7c2d731db7856b4c2f0affeb372aa0
anonymousRecords <a.Records2022@gmail.com>
2022-09-15 21:04:17 +0900

    p6070

--- 2회독/p6070.py
+++ 2회독/p6070.py
@@ -1,6 +1,9 @@
 month = int(input())
-# if (month == 12) & (month ==1) & (month == 2):
-if month == 12 & month == 1 & month == 2:
-    print('spring')
+if month//3==1:
+    print("spring")
+elif month//3==2:
+    print("summer")
+elif month//3==3:
+    print("fall")
 else:
-    print('not')
+    print("winter")
